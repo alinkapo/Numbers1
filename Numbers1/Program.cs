414c2e87400b3a882c0bb0f17da0d91d9d6bb817
Андрей <@DESKTOP-USER>
2022-10-18 17:20:44 +0400

    Добавьте файлы проекта.

---
+++ Numbers1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Numbers1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] array = new int[6] { 5,7,9,34,-2,-8};
+          
+            Array.Sort(array);
+            if (array.Length > 0)
+            {
+                Console.WriteLine("Минимальное число: " + array[0]);
+                Console.WriteLine("Максимальное число: " + array[array.Length - 1]);
+            }
+        }
+    }
+}
